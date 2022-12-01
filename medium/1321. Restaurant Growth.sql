dad331d494b4189ead57cd11233f6abb44ec2ad7
Chintan <cmistry291@gmail.com>
2022-12-01 10:16:34 +0530

    Adding new Solutions

--- medium/1321. Restaurant Growth.sql
+++ medium/1321. Restaurant Growth.sql
@@ -16,3 +16,23 @@ cte2 AS(
 SELECT visited_on,sum_amount,average_amount
 FROM cte2
 WHERE num>=7;
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+WITH daily_spent AS (
+	SELECT visited_on,SUM(amount) AS amount,
+		ROW_NUMBER() OVER (ORDER BY visited_on) AS rn
+	FROM customer_1321
+	GROUP BY visited_on
+),
+moving_averages AS (
+	SELECT visited_on,rn,
+		ROUND((AVG(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)),2) AS running_avg
+	FROM daily_spent
+	ORDER BY visited_on
+)
+SELECT *
+FROM moving_averages
+WHERE rn >= 7;
+
+-- Window functions are evaluated after group by clause got executed, and window functions are applied to the result of group by clause.
