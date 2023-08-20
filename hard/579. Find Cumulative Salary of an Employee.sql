db833fb4773e61c302b73d9ed11fa2b38bb67291
Chintan Mistri <cmistry291@gmail.com>
2023-08-20 18:24:40 +0530

    Update 579. Find Cumulative Salary of an Employee.sql

--- hard/579. Find Cumulative Salary of an Employee.sql
+++ hard/579. Find Cumulative Salary of an Employee.sql
@@ -12,3 +12,12 @@ SELECT id,month,salary
 FROM cte
 WHERE row_num<count
 ORDER BY id,month DESC;
+
+----------------------------- OR -----------------------------
+
+SELECT e1.id,e1.month,SUM(e2.salary)
+FROM employee_579 e1
+JOIN employee_579 e2 ON e1.id = e2.id AND e1.month - e2.month BETWEEN 0 AND 2
+WHERE (e1.id,e1.month) NOT IN (SELECT id,MAX(MONTH) FROM employee_579 GROUP BY id)
+GROUP BY e1.id,e1.month
+ORDER BY e1.id,e1.month DESC;
