69df39bacbcc8e438687aaec9fb58344d6ebb49e
Chintan Mistri <cmistry291@gmail.com>
2023-08-20 17:41:59 +0530

    Update 569. Median Employee Salary.sql
    Updated Solution

--- hard/569. Median Employee Salary.sql
+++ hard/569. Median Employee Salary.sql
@@ -1,13 +1,8 @@
-WITH sorted AS(
-	SELECT *,
-		ROW_NUMBER() OVER w1 AS row_num,
-		COUNT(*) OVER w2 AS count
+WITH cte AS (
+	SELECT id,company,salary,
+		ABS(ROW_NUMBER() OVER (PARTITION BY company ORDER BY salary,id) - ROW_NUMBER() OVER (PARTITION BY company ORDER BY salary DESC,id DESC)) AS diff
 	FROM employee_569
-	WINDOW w1 AS (PARTITION BY company ORDER BY salary),
-		w2 AS (PARTITION BY company)
 )
-
-SELECT company,ROUND(AVG(salary),2)
-FROM sorted
-WHERE row_num BETWEEN count::NUMERIC/2 AND count::NUMERIC/2 + 1
-GROUP BY company;
+SELECT id,company,salary
+FROM cte 
+WHERE diff = 0 OR diff = 1;
