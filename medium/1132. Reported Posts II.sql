c58852eb130002836bed753f963397fe44fe0cdd
Chintan Mistri <cmistry291@gmail.com>
2023-08-23 08:04:50 +0530

    Update 1132. Reported Posts II.sql

--- medium/1132. Reported Posts II.sql
+++ medium/1132. Reported Posts II.sql
@@ -12,3 +12,16 @@ percentage AS(
 )
 
 SELECT ROUND(AVG(per),2) AS avg_daily_percent FROM percentage;
+
+--------------------------- OR ---------------------------
+
+WITH cte AS (
+	SELECT a.action_date,
+			ROUND(COUNT(CASE WHEN a.extra = 'spam' AND r.post_id IS NOT NULL THEN 1 ELSE NULL END)*100::NUMERIC/COUNT(DISTINCT a.post_id),2) AS removed_spammed_post_percentage
+	FROM actions_1132 a
+	LEFT JOIN removals_1132 r ON a.post_id = r.post_id
+	GROUP BY a.action_date
+)
+SELECT ROUND(AVG(removed_spammed_post_percentage),2)
+FROM cte
+WHERE removed_spammed_post_percentage <> 0;
