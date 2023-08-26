d5742aafecf161beb5de936cb75d65467520e37d
Chintan Mistri <cmistry291@gmail.com>
2023-08-26 09:26:50 +0530

    Update 1270. All People Report to the Given Manager.sql

--- medium/1270. All People Report to the Given Manager.sql
+++ medium/1270. All People Report to the Given Manager.sql
@@ -37,3 +37,18 @@ ON e1.manager_id = e2.employee_id
 INNER JOIN employees_1270 e3 
 ON e2.manager_id = e3.employee_id 
 WHERE e3.manager_id = 1 AND e1.employee_id <> 1
+
+
+--------------(OR)------------
+
+WITH RECURSIVE cte AS (
+	SELECT employee_id,employee_name,manager_id,1 AS level
+	FROM employees_1270
+	WHERE employee_id = 1
+	UNION
+	SELECT e.employee_id,e.employee_name,e.manager_id,level+1 AS level
+	FROM cte c
+	INNER JOIN employees_1270 e ON c.employee_id = e.manager_id
+	WHERE level < 4
+)
+SELECT DISTINCT employee_id,employee_name FROM cte WHERE employee_id <> 1;
